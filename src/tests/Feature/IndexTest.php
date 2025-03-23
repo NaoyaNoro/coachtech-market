e35357e86736ec2086a13772a2a2c3067735c041
naoya noro <naoya20180830@gmail.com>
2025-03-24 06:48:43 +0900

    phpUnitTestの修正

--- src/tests/Feature/IndexTest.php
+++ src/tests/Feature/IndexTest.php
@@ -1,6 +1,82 @@
 <?php
 
-namespace Tests\Feature;
+// namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+// use App\Models\Product;
+// use App\Models\User;
+// use App\Models\Purchase;
+// use App\Models\Sell;
+// use Illuminate\Http\Response;
+
+// class IndexTest extends TestCase
+// {
+//     use RefreshDatabase;
+
+//     //全商品を取得できる
+//     public function test_get_all_products_date()
+//     {
+//         Product::factory()->count(3)->create([
+//             'name'=>'テスト商品',
+//             'image'=>'test-image.jpg'
+//         ]);
+//         $response=$this->get('/');
+//         $response->assertStatus(Response::HTTP_OK);
+//         $products=Product::all();
+//         foreach($products as $product){
+//             $response->assertSee($product->name);
+//             $response->assertSee($product->image);
+//         }
+//     }
+
+//     //Soldと表示される
+//     public function test_sold_out_label()
+//     {
+//         $user=User::factory()->create();
+//         $productPurchased=Product::factory()->create();
+//         $productNotPurchased=Product::factory()->create();
+
+//         Purchase::factory()->create([
+//             'user_id'=>$user->id,
+//             'product_id'=>$productPurchased->id,
+//             'post_code' => '160-0023',
+//             'address' => '東京都新宿区西新宿1-2-3',
+//             'building' => '新宿グランドタワー 15階'
+//         ]);
+
+//         $response=$this->get('/');
+//         $response->assertSee($productPurchased->name);
+//         $response->assertSee($productPurchased->image);
+//         $response->assertSee($productNotPurchased->name);
+//         $response->assertSee($productNotPurchased->image);
+
+//         $response->assertSeeInOrder([
+//             $productPurchased->name,
+//             'Sold'
+//         ]);
+//     }
+
+//     //自分が出品した商品は表示されない
+//     public function test_sell_product_no_see()
+//     {
+//         $user=User::factory()->create()->first();
+//         $productSell=Product::factory()->create();
+//         $productNotSell = Product::factory()->create();
+
+//         Sell::factory()->create([
+//             'user_id'=>$user->id,
+//             'product_id'=>$productSell->id,
+//         ]);
+
+//         $response = $this->actingAs($user)->get('/');
+//         $response->assertSee($productNotSell->name);
+//         $response->assertSee($productNotSell->image);
+//         $response->assertDontSee($productSell->name);
+//         $response->assertDontSee($productSell->image);
+//     }
+// }
+
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -18,13 +94,13 @@ class IndexTest extends TestCase
     public function test_get_all_products_date()
     {
         Product::factory()->count(3)->create([
-            'name'=>'テスト商品',
-            'image'=>'test-image.jpg'
+            'name' => 'テスト商品',
+            'image' => 'test-image.jpg'
         ]);
-        $response=$this->get('/');
+        $response = $this->get('/');
         $response->assertStatus(Response::HTTP_OK);
-        $products=Product::all();
-        foreach($products as $product){
+        $products = Product::all();
+        foreach ($products as $product) {
             $response->assertSee($product->name);
             $response->assertSee($product->image);
         }
@@ -33,19 +109,19 @@ class IndexTest extends TestCase
     //Soldと表示される
     public function test_sold_out_label()
     {
-        $user=User::factory()->create();
-        $productPurchased=Product::factory()->create();
-        $productNotPurchased=Product::factory()->create();
+        $user = User::factory()->create();
+        $productPurchased = Product::factory()->create();
+        $productNotPurchased = Product::factory()->create();
 
         Purchase::factory()->create([
-            'user_id'=>$user->id,
-            'product_id'=>$productPurchased->id,
+            'user_id' => $user->id,
+            'product_id' => $productPurchased->id,
             'post_code' => '160-0023',
             'address' => '東京都新宿区西新宿1-2-3',
             'building' => '新宿グランドタワー 15階'
         ]);
 
-        $response=$this->get('/');
+        $response = $this->get('/');
         $response->assertSee($productPurchased->name);
         $response->assertSee($productPurchased->image);
         $response->assertSee($productNotPurchased->name);
@@ -60,13 +136,19 @@ class IndexTest extends TestCase
     //自分が出品した商品は表示されない
     public function test_sell_product_no_see()
     {
-        $user=User::factory()->create()->first();
-        $productSell=Product::factory()->create();
-        $productNotSell = Product::factory()->create();
+        $user = User::factory()->create()->first();
+        $productSell = Product::factory()->create([
+            'name' => '商品_非表示',
+            'image' => 'test-sell.jpg'
+        ]);
+        $productNotSell = Product::factory()->create([
+            'name' => '商品_表示',
+            'image' => 'test-not-sell.jpg'
+        ]);
 
         Sell::factory()->create([
-            'user_id'=>$user->id,
-            'product_id'=>$productSell->id,
+            'user_id' => $user->id,
+            'product_id' => $productSell->id,
         ]);
 
         $response = $this->actingAs($user)->get('/');
--- src/tests/Feature/MyListTest.php
+++ src/tests/Feature/MyListTest.php
@@ -1,5 +1,111 @@
 <?php
 
+// namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+// use App\Models\Product;
+// use App\Models\User;
+// use App\Models\MyList;
+// use App\Models\Purchase;
+// use App\Models\Sell;
+
+// class MyListTest extends TestCase
+// {
+//     use RefreshDatabase;
+//     /**
+//      * A basic feature test example.
+//      *
+//      * @return void
+//      */
+//     //いいねした商品だけが表示される
+//     public function test_my_list_product(){
+//         $user = User::factory()->create()->first();
+//         $productMyList = Product::factory()->create();
+//         $productNotMyList= Product::factory()->create();
+//         $this->actingAs($user);
+//         MyList::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $productMyList->id,
+//         ]);
+
+//         $response = $this->get('/?page=mylist');
+//         $response->assertSee($productMyList->name);
+//         $response->assertSee($productMyList->image);
+//         $response->assertDontSee($productNotMyList->name);
+//         $response->assertDontSee($productNotMyList->image);
+//     }
+
+//     //Soldが表示される
+//     public function test_my_list_product_sold_out()
+//     {
+//         $user = User::factory()->create()->first();
+//         $productMyListSoldOut = Product::factory()->create();
+//         $productMyListNotSoldOut = Product::factory()->create();
+//         $this->actingAs($user);
+//         MyList::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $productMyListSoldOut->id,
+//         ]);
+
+//         MyList::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $productMyListNotSoldOut->id,
+//         ]);
+
+//         Purchase::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $productMyListSoldOut->id,
+//             'post_code' => '160-0023',
+//             'address' => '東京都新宿区西新宿1-2-3',
+//             'building' => '新宿グランドタワー 15階'
+//         ]);
+
+//         $response = $this->get('/?page=mylist');
+
+//         $response->assertSeeInOrder([
+//             $productMyListSoldOut->name,
+//             'Sold'
+//         ]);
+//         $response->assertSee($productMyListNotSoldOut->name);
+//     }
+
+//     //自分が出品した商品は表示されない
+//     public function test_sell_product_no_see()
+//     {
+//         $user = User::factory()->create()->first();
+//         $productSell = Product::factory()->create();
+
+//         Sell::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $productSell->id,
+//         ]);
+
+
+//         $response = $this->actingAs($user)->get('/?page=mylist');
+//         $response->assertDontSee($productSell->name);
+//         $response->assertDontSee($productSell->image);
+//     }
+
+//     //未認証の場合は、何も表示されない
+//     public function test_no_users_cannot_see_mylist()
+//     {
+//         $user = User::factory()->create();
+//         $product = Product::factory()->create();
+//         MyList::factory()->create([
+//             'user_id' => $user->id,
+//             'product_id' => $product->id,
+//         ]);
+
+//         $response = $this->get('/?page=mylist');
+//         $response->assertStatus(200);
+//         $response->assertSee('マイリスト');
+//         $response->assertDontSee($product->name);
+//         $response->assertDontSee($product->image);
+//     }
+// }
+
+
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,10 +125,17 @@ class MyListTest extends TestCase
      * @return void
      */
     //いいねした商品だけが表示される
-    public function test_my_list_product(){
+    public function test_my_list_product()
+    {
         $user = User::factory()->create()->first();
-        $productMyList = Product::factory()->create();
-        $productNotMyList= Product::factory()->create();
+        $productMyList = Product::factory()->create([
+            'name' => '商品_表示',
+            'image' => 'test-mylist.jpg'
+        ]);
+        $productNotMyList = Product::factory()->create([
+            'name' => '商品_非表示',
+            'image' => 'test-not-mylist.jpg'
+        ]);
         $this->actingAs($user);
         MyList::factory()->create([
             'user_id' => $user->id,
@@ -74,7 +187,10 @@ class MyListTest extends TestCase
     public function test_sell_product_no_see()
     {
         $user = User::factory()->create()->first();
-        $productSell = Product::factory()->create();
+        $productSell = Product::factory()->create([
+            'name' => '商品_非表示',
+            'image' => 'test-not-sell.jpg'
+        ]);
 
         Sell::factory()->create([
             'user_id' => $user->id,
@@ -91,7 +207,10 @@ class MyListTest extends TestCase
     public function test_no_users_cannot_see_mylist()
     {
         $user = User::factory()->create();
-        $product = Product::factory()->create();
+        $product = Product::factory()->create([
+            'name' => '商品_非表示',
+            'image' => 'test.jpg'
+        ]);
         MyList::factory()->create([
             'user_id' => $user->id,
             'product_id' => $product->id,
